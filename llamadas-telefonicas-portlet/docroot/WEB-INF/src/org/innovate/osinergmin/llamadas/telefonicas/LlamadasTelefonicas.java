b7832358eb138aade2123e958efd5d34f3f180c0
mrisco <mrisco@innovate.pe>
2012-03-22 13:16:33 -0500

    Creación del Portlet de Llamdas Telefonicas

---
+++ llamadas-telefonicas-portlet/docroot/WEB-INF/src/org/innovate/osinergmin/llamadas/telefonicas/LlamadasTelefonicas.java
@@ -0,0 +1,11 @@
+package org.innovate.osinergmin.llamadas.telefonicas;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class LlamadasTelefonicas
+ */
+public class LlamadasTelefonicas extends MVCPortlet {
+ 
+
+}
